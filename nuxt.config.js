46bf658c44ee826441031d15aa9863784634deab
ggsakurai <sakurai@kdl.co.jp>
2021-04-07 13:49:09 +0000

    after contentful cooperation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,32 @@
+const pkg = require('./package')
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+
+const { createClient } = require('./plugins/contentful.js')
+const cdaClient = createClient(ctfConfig)
+
+module.exports = {
+  // 省略、、、
+  generate: {
+    routes() {
+      return cdaClient
+        .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+        .then(entries => {
+          return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)]
+        })
+    }
+  },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
+  }
+}
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
